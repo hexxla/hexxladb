cc0da9b2acdb4e01606d3c5f0a7fac2cc0ab384d
sploitzberg <sploitzberg@proton.me>
2026-05-09 03:50:58 +0100

    fix: exclude gosec G115 for primary_datasync_linux via golangci config

--- internal/engine/primary_datasync_linux.go
+++ internal/engine/primary_datasync_linux.go
@@ -9,5 +9,5 @@ import (
 )
 
 func primaryDataSyncFile(f *os.File) error {
-	return unix.Fdatasync(int(f.Fd())) //nolint:gosec // G115: int(fd) safe on Linux; FD fits int
+	return unix.Fdatasync(int(f.Fd()))
 }
